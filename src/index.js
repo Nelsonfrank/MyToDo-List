a5d605e818c21ac4dc767e05aa93646ac953158f
Your Name <you@example.com>
2018-09-06 10:44:25 +0300

    changes made

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
 import Containers from './components/Containers';
 
 ReactDOM.render(<Containers />, document.getElementById("root"));
