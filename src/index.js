00bd653ff177a06405328d7bbbc8b76734c980ce
Your Name <you@example.com>
2018-11-02 07:10:06 +0300

    app upgrade

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import '/home/nelson/mytodo-list/node_modules/bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
